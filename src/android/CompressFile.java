1576e792425dc3ea4e70421edb241b83700d9c55
Nayyar Jamal <nayyarjamal@innotical.com>
2018-07-28 15:08:08 +0530

    android compression code

--- src/android/CompressFile.java
+++ src/android/CompressFile.java
@@ -1,4 +1,4 @@
-package cordova-plugin-file-compression;
+package com.nayyar.cordova.CompressFile;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -6,6 +6,21 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.media.ExifInterface;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 /**
  * This class echoes a string called from JavaScript.
@@ -18,6 +33,23 @@ public class CompressFile extends CordovaPlugin {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
+        } else if (action.equals("compressImage")) {
+            String message = args.getString(0);
+            String folderPath = args.getString(1);
+            int compressionSize = args.getInt(2);
+
+            //compressionSize
+            if(compressionSize == 0){
+                compressionSize = 80;
+            }
+
+            //folderPath
+            if(folderPath == null){
+                folderPath = "MyFolder/Images";
+            }
+
+            this.compressImage(message, folderPath, compressionSize, callbackContext);
+            return true;
         }
         return false;
     }
@@ -29,4 +61,178 @@ public class CompressFile extends CordovaPlugin {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+    public void compressImage(String resultPath, String folderPath, int compressionSize, CallbackContext callbackContext) {
+
+        if (resultPath != null && resultPath.length() > 0) {
+            //String filePath = getRealPathFromURI(imageUri);
+            
+            String filePath = resultPath;
+            Bitmap scaledBitmap = null;
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+
+            //by setting this field as true, the actual bitmap pixels are not loaded in the memory. Just the bounds are loaded. If
+            //you try the use the bitmap here, you will get null.
+            
+            options.inJustDecodeBounds = true;
+            Bitmap bmp = BitmapFactory.decodeFile(filePath, options);
+
+            int actualHeight = options.outHeight;
+            int actualWidth = options.outWidth;
+
+            //max Height and width values of the compressed image is taken as 816x612
+
+            float maxHeight = 816.0f;
+            float maxWidth = 612.0f;
+            float imgRatio = actualWidth / actualHeight;
+            float maxRatio = maxWidth / maxHeight;
+
+            //width and height values are set maintaining the aspect ratio of the image
+
+            if (actualHeight > maxHeight || actualWidth > maxWidth) {
+
+                if (imgRatio < maxRatio) {
+
+                    imgRatio = maxHeight / actualHeight;
+                    actualWidth = (int) (imgRatio * actualWidth);
+                    actualHeight = (int) maxHeight;
+
+                } else if (imgRatio > maxRatio) {
+
+                    imgRatio = maxWidth / actualWidth;
+                    actualHeight = (int) (imgRatio * actualHeight);
+                    actualWidth = (int) maxWidth;
+                    
+                } else {
+
+                    actualHeight = (int) maxHeight;
+                    actualWidth = (int) maxWidth;
+
+                }
+            }
+
+            //setting inSampleSize value allows to load a scaled down version of the original image
+
+            options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
+
+            //inJustDecodeBounds set to false to load the actual bitmap
+            options.inJustDecodeBounds = false;
+
+            //this options allow android to claim the bitmap memory if it runs low on memory
+            options.inPurgeable = true;
+            options.inInputShareable = true;
+            options.inTempStorage = new byte[16 * 1024];
+
+            //load the bitmap from its path
+            try {
+                bmp = BitmapFactory.decodeFile(filePath, options);
+            } catch (OutOfMemoryError exception) {
+                exception.printStackTrace();
+            }
+
+            try {
+                scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888);
+            } catch (OutOfMemoryError exception) {
+                exception.printStackTrace();
+            }
+
+            float ratioX = actualWidth / (float) options.outWidth;
+            float ratioY = actualHeight / (float) options.outHeight;
+            float middleX = actualWidth / 2.0f;
+            float middleY = actualHeight / 2.0f;
+
+            Matrix scaleMatrix = new Matrix();
+            scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+
+            Canvas canvas = new Canvas(scaledBitmap);
+            canvas.setMatrix(scaleMatrix);
+            canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
+
+            //check the rotation of the image and display it properly
+            ExifInterface exif;
+
+            try {
+                exif = new ExifInterface(filePath);
+                int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0);
+                Log.d("EXIF", "Exif: " + orientation);
+                Matrix matrix = new Matrix();
+
+                if (orientation == 6) {
+
+                    matrix.postRotate(90);
+                    Log.d("EXIF", "Exif: " + orientation);
+
+                } else if (orientation == 3) {
+
+                    matrix.postRotate(180);
+                    Log.d("EXIF", "Exif: " + orientation);
+
+                } else if (orientation == 8) {
+
+                    matrix.postRotate(270);
+                    Log.d("EXIF", "Exif: " + orientation);
+
+                }
+
+                scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            FileOutputStream out = null;
+            String filename = getFilename(folderPath);
+    
+            //write the compressed bitmap at the destination specified by filename.
+
+            try {
+                out = new FileOutputStream(filename);
+                scaledBitmap.compress(Bitmap.CompressFormat.JPEG, compressionSize, out);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }        
+
+            callbackContext.success(filename);
+        } else {
+            callbackContext.error("File not found.");
+        }
+
+        // return filename;
+
+    }
+
+
+    public String getFilename(String folderPath) {
+        File file = new File(Environment.getExternalStorageDirectory().getPath(), folderPath);
+        
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        
+        String uriSting = (file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".jpg");
+        return uriSting;
+ }
+
+ public int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    final int height = options.outHeight;
+    final int width = options.outWidth;
+    int inSampleSize = 1;
+
+    if (height > reqHeight || width > reqWidth) {
+        final int heightRatio = Math.round((float) height / (float) reqHeight);
+        final int widthRatio = Math.round((float) width / (float) reqWidth);
+        inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+    }
+
+    final float totalPixels = width * height;
+    final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+    
+    while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+        inSampleSize++;
+    }
+
+    return inSampleSize;
+ }
+
 }
--- www/CompressFile.js
+++ www/CompressFile.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'CompressFile', 'coolMethod', [arg0]);
 };
+
+exports.compressImage = function (filePath, folderPath, compressionSize, success, error) {
+    exec(success, error, 'CompressFile', 'compressImage', [filePath, folderPath, compressionSize]);
+};
